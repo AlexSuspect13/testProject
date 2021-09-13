22ada1643023b350de351be5a296ad74e176ba03
Your name <pmodelist@bk.ru>
2021-09-13 14:33:27 +0300

    testproj

---
+++ src/components/Table/table.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { useTable } from "react-table";
+import CoLumns from "../columns.js";
+import Many from "../../Many.json";
+
+import "./table.css"
+
+const MyTable = () => {
+  const columns = React.useMemo(() => CoLumns, []);
+
+  const data = React.useMemo(() => Many, []);
+
+  const tableInstance = useTable({ columns, data });
+
+  const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return <td{...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default MyTable;
---
+++ src/components/columns.js
@@ -0,0 +1,29 @@
+const CoLumns =  [
+    {
+        Header: "Cur_ID",
+        accessor: "Cur_ID"
+    },
+    {
+        Header: "Date",
+        accessor: "Date"
+    },
+    {
+        Header: "Cur_Abbreviation",
+        accessor: "Cur_Abbreviation"
+    },
+    {
+        Header: "Cur_Scale",
+        accessor: "Cur_Scale"
+    },
+    {
+        Header: "Cur_Name",
+        accessor: "Cur_Name"
+    },
+    {
+        Header: "Cur_OfficialRate",
+        accessor: "Cur_OfficialRate"
+    }
+]
+
+
+export default CoLumns;
